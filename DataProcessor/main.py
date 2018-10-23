b176e6fff7df275460d5ad4f27d1799269d2fccc
Sea dragon King <tsinghua_phd@163.com>
2018-10-23 16:06:43 +0800

    first commit

---
+++ DataProcessor/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from const import *
+from variable import *
+from property import *
+from CL import *
+from ontology import *
+from typepath import *
+from text import *
+
+if __name__ == "__main__":
+	#get_property_list()
+	#get_cross_lingual_links()
+	#get_taxonomy_info()
+	#get_instance_typepath()
+	get_tfidf_feature()
